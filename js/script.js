bfb7426a0cfa71532f25742f13b7844f6eef6956
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-14 17:31:15 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ain Jeong
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function counts the number of clicking of cookie
+ */
+// eslint-disable-next-line no-unused-vars
+function cookieClicker () {
+  // input
+  let cookieClick = localStorage.getItem('cookieClick')
+
+    if (cookieClick >= 1) {
+    cookieClick = Number(cookieClick) + 1
+  } else {
+    cookieClick = 1
+  }
+
+  cookieClick = localStorage.getItem('cookieClick')
+
+  // output
+  document.getElementById('answer').innerHTML = cookieClick + ' Cookies'
+}
